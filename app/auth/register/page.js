8321858e099a5ddc1414c1bf9bc59f4d7b349e2d
wizarx <thanhtikezaw404@gmail.com>
2024-03-11 23:29:41 +0630

    second commit

---
+++ app/auth/register/page.js
@@ -0,0 +1,81 @@
+'use client'
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import   { useRouter } from "next/navigation";
+export default function Register() {
+    const router=useRouter()
+    const [formData, setFormData] = useState({
+    email: '',
+    password: ''
+  });
+
+    if(localStorage.getItem('token') !== null) {
+     router.push('/')
+    }
+
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit =async (e) => {
+    e.preventDefault();
+    const response=await axios.post('http://localhost:4001/register',formData)
+    console.log(response.data)
+    if(response.data.token){
+      localStorage.setItem('token',response.data.token)
+      router.push('/')  
+    }else{
+      alert('Something went wrong')
+    }
+   
+  };
+
+  return (
+    <div className="min-h-screen flex justify-center items-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+      <div className="max-w-md w-full space-y-8">
+        <div>
+          <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Register</h2>
+        </div>
+        <form className="mt-8 space-y-6" onSubmit={handleSubmit}>
+          <div className="rounded-md shadow-sm -space-y-px space-y-4">
+            <div>
+              <label htmlFor="email" className="sr-only">Email address</label>
+              <input
+                id="email"
+                name="email"
+                type="email"
+                autoComplete="email"
+                required
+                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-none focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                placeholder="Email address"
+                value={formData.email}
+                onChange={handleChange}
+              />
+            </div>
+            <div>
+              <label htmlFor="password" className="sr-only">Password</label>
+              <input
+                id="password"
+                name="password"
+                type="password"
+                autoComplete="current-password"
+                required
+                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                placeholder="Password"
+                value={formData.password}
+                onChange={handleChange}
+              />
+            </div>
+          </div>
+          <div>
+            <button type="submit" className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+              Register
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
